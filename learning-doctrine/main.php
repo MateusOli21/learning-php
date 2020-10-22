2c7c977feaf837dcc9e94af86380e6186ab22a20
MateusOli21 <mateus.oli.abreu@hotmail.com>
2020-10-22 19:24:00 -0300

    Initial commit.

---
+++ learning-doctrine/main.php
@@ -0,0 +1,10 @@
+<?php
+
+use Mateus\Doctrine\helper\EntityManagerFactory;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+var_dump($entityManager->getConnection());
